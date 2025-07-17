1634f2e3e9097c12c35acc430990a41edb8f7513
Otis Nguyen <0943244904hung@gmail.com>
2025-07-18 01:20:28 +0700

    ;

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
-
-const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  await app.init(); // ✅ cần có "await" ở đây!
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-export default expressApp; // ✅ rất quan trọng để Vercel có thể dùng
