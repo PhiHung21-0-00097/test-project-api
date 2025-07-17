9a190c661e8468b5ce3a72e49399ebd2799b2435
Otis Nguyen <0943244904hung@gmail.com>
2025-07-18 01:01:02 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
+const server = express();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule , new ExpressAdapter(server));
+  app.init()
 }
 bootstrap();
+
+export default server;
